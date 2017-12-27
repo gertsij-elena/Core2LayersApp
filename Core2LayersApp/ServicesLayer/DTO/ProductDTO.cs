692247b9263a75b6b0e4f1ccc55a819fcddfbbce
elena-gertsij <gertsij.elena@yandex.ua>
2017-12-27 13:26:26 +0200

    add ProducttDTO

---
+++ Core2LayersApp/ServicesLayer/DTO/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Core2LayerApp.Services.DTO
+{
+    class ProductDTO
+    {
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string TypeDescription { get; set; }
+        public int CountCategory { get; set; }
+
+    }
+}
